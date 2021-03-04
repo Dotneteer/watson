a4a6d85b6d2c2eb101b86406992acbcef845305b
Dotneteer <dotneteer@hotmail.com>
2021-03-04 13:58:30 +0100

    build: compiler moved to the packages/watsharp-core folder

---
+++ packages/watsharp-core/src/index.ts
@@ -0,0 +1,1 @@
+export { WatSharpCompiler } from "./compiler/WatSharpCompiler";
---
+++ packages/watsharp-core/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+const libraryName = "whatsharp-core";
+const srcFolder = "./src";
+const entrySrcName = `index.ts`;
+const buildOutputFolder = "";
+const bundleName = "index.js";
+
+module.exports = {
+  entry: path.resolve(srcFolder, entrySrcName),
+  mode: "development",
+  output: {
+    filename: bundleName,
+    path: path.resolve(__dirname, buildOutputFolder),
+    library: libraryName,
+    libraryTarget: "umd",
+  },
+  target: "node",
+  devtool: "source-map",
+  resolve: {
+    extensions: [".ts", ".js", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+        options: { configFile: "../tsconfig.build.json" },
+      },
+    ],
+  },
+  plugins: [
+    // --- Add plugins later
+  ],
+};
